03f16075190e37f88d2f613d8c2e376edd73a46b
Ransomwave <alexandrutudor2504@gmail.com>
2025-04-09 17:37:31 +0200

    fix: update background gradients in GridBackground component for improved visibility and styling

--- src/components/GridBg.tsx
+++ src/components/GridBg.tsx
@@ -8,12 +8,12 @@ export function GridBackground({ children }: { children: React.ReactNode }) {
         className={cn(
           "absolute inset-0",
           "[background-size:40px_40px]",
-          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+          // "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "[background-image:linear-gradient(to_right,#2e2e2e_1px,transparent_1px),linear-gradient(to_bottom,#2e2e2e_1px,transparent_1px)]"
         )}
       />
       {/* Radial gradient for the container to give a faded look */}
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] bg-[#1e1f22]"></div>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)] bg-[#1e1f22]"></div>
       <div className="relative z-20 bg-gradient-to-b from-neutral-50 to-red-600 bg-clip-text py-8 text-4xl font-bold sm:text-7xl">
         {children}
       </div>

3078a8782485e9c89546ecd8f0d3b653df3e2678
Ransomwave <alexandrutudor2504@gmail.com>
2025-04-11 10:44:27 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,13 +21,13 @@ export default function Home() {
       <Section
         title="We fixed remote access"
         description="Accessing your devices & transfering files has never been simpler. Securely and conveniently, from anywhere in the world."
-        image="/screenshots/app.webp"
+        image="/screenshots/app1.png"
       ></Section>
 
       <Section
         title="Built from the ground up with your security in mind"
         description="Never expose your home network to the internet again. Enjoy a blazingly fast & secure experience."
-        image="/screenshots/app.webp"
+        image="/screenshots/app2.png"
       ></Section>
 
       <CarouselComp

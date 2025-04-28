23f2bf2826103b6f76c294f6d9fa2e1dbdb482bf
luqueee <luqueee_@outlook.es>
2025-04-28 10:02:33 +0200

    feat: add loading state to DropZone component using Suspense

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,9 +1,12 @@
 import { DropZone } from "@/components/uploader/DropZone";
-export default async function Upload() {
+import { Suspense } from "react";
+export default function Upload() {
   return (
     <div className="pt-24 h-screen">
       <div className="md:lg:w-[40%] w-full mx-auto">
-        <DropZone />
+        <Suspense fallback={<div>Loading...</div>}>
+          <DropZone />
+        </Suspense>
       </div>
     </div>
   );

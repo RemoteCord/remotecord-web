3c9f985e52175db9c081567cc000f889837b48ff
luqueee <luqueee_@outlook.es>
2025-04-28 09:58:05 +0200

    refactor: simplify Redirect function by removing unused parameters and error handling

--- src/app/discord/page.tsx
+++ src/app/discord/page.tsx
@@ -4,12 +4,6 @@ import { redirect } from "next/navigation";
 
 const { NEXT_PUBLIC_DISCORD_INVITE_URL } = process.env;
 
-export default async function Redirect(id: string) {
-  try {
-    // Call database
-  } catch (error) {
-    // Handle errors
-  }
-
+export default async function Redirect() {
   redirect(NEXT_PUBLIC_DISCORD_INVITE_URL as string); // Navigate to the new post page
 }
